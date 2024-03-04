7ebb6cf64f55819a1faad054c15fe73a13278e2f
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-03-04 08:45:11 -0800

    Alert Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -22,30 +25,45 @@ MySnackBar(message,context){
   return ScaffoldMessenger.of(context).showSnackBar(SnackBar
     (content:Text(message)));
 }
-
+MyAlertDialog(context){
+  showDialog(context: context,
+      builder: (BuildContext context){
+    return Expanded(child:
+    AlertDialog(
+title: const Text("Alert !"),
+content: const Text("Do You Want to Deleted"),
+actions: [
+  TextButton(onPressed: (){MySnackBar("Deleted Sucess",context );Navigator.of(context).pop();}, child: const Text("Yes")),
+  TextButton(onPressed: (){Navigator.of(context).pop();}, child: const Text("No")),
+],
+    ),);
+  });
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Mahadi App"),
+      appBar: AppBar(title:const Text("Mahadi App"),
 
       backgroundColor:Colors.teal,
         elevation: 2,
         toolbarHeight:70,
         titleSpacing: 35,
         actions: [
-          IconButton(onPressed: (){MySnackBar("This is Camera ", context);}, icon:Icon(Icons.add_a_photo)),
-          IconButton(onPressed: (){MySnackBar("This is Searce Bar", context);}, icon:Icon(Icons.search)),
-          IconButton(onPressed: (){MySnackBar("This is More Button", context);}, icon:Icon(Icons.more_vert))
+          IconButton(onPressed: (){MySnackBar("This is Camera ", context);}, icon:const Icon(Icons.add_a_photo)),
+          IconButton(onPressed: (){MySnackBar("This is Searce Bar", context);}, icon:const Icon(Icons.search)),
+          IconButton(onPressed: (){MySnackBar("This is More Button", context);}, icon:const Icon(Icons.more_vert))
         ],
       ),
-floatingActionButton: FloatingActionButton(onPressed: (){},child:Icon(Icons.add,color: Colors.white,),
+floatingActionButton: FloatingActionButton(onPressed: (){},
+
   backgroundColor:(Colors.black54),
   elevation: 10,
+  child:const Icon(Icons.add,color: Colors.white,),
 ),
 
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 2,
-          items: [
+          items: const[
              BottomNavigationBarItem(icon: Icon(Icons.add_call,color: Colors.black,), label: "Call Now",backgroundColor: Colors.teal),
             BottomNavigationBarItem(icon: Icon(Icons.add_alert_rounded,color: Colors.black,), label: "Notification",backgroundColor: Colors.teal),
             BottomNavigationBarItem(icon: Icon(Icons.home,color: Colors.black,), label: "Home",backgroundColor: Colors.teal),
@@ -65,29 +83,29 @@ floatingActionButton: FloatingActionButton(onPressed: (){},child:Icon(Icons.add,
         child: ListView(
           children:[
           DrawerHeader(
-            padding: EdgeInsets.all(0),
+            padding: const EdgeInsets.all(0),
 
              child: UserAccountsDrawerHeader(
-               decoration: BoxDecoration(color: Colors.black),
-                 accountName: Text("Mahadi Hasan"),
-          accountEmail:Text("mahadib2b@gmail.com"),
+               decoration: const BoxDecoration(color: Colors.black),
+                 accountName:const Text("Mahadi Hasan"),
+          accountEmail:const Text("mahadib2b@gmail.com"),
                currentAccountPicture: Image.network("https://avatars.githubusercontent.com/u/153096844?…00&u=d9c6f32ad4ec53bf7449faccdec60ac21f2bbc14&v=4"),
-               currentAccountPictureSize: const Size.square(60.0),
+
              ),
              ),
             ListTile(
-              leading:Icon(Icons.add_alert_rounded),
-              title: Text("Notification"),
+              leading: const Icon(Icons.add_alert_rounded),
+              title:const Text("Notification"),
               onTap: (){},
             ),
             ListTile(
-              leading:Icon(Icons.add_call),
-              title:Text("Call Now"),
+              leading:const Icon(Icons.add_call),
+              title:const Text("Call Now"),
               onTap:(){},
             ),
             ListTile(
-              leading:Icon(Icons.add_a_photo),
-            title:Text("Camera"),
+              leading:const Icon(Icons.add_a_photo),
+            title:const Text("Camera"),
               onTap:(){},
             ),
 
@@ -96,7 +114,22 @@ floatingActionButton: FloatingActionButton(onPressed: (){},child:Icon(Icons.add,
 
         ),
       ),
-
+      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(height: 250,width: 250,
+            child:  ElevatedButton(onPressed: (){},child: const Text("Alert"),),
+          alignment: Alignment.center,
+padding:const EdgeInsets.all(40),
+            margin:const EdgeInsets.all(70),
+            decoration:
+            BoxDecoration(
+                color: Colors.amberAccent,
+                border: Border.all(color:Colors.cyan,width: 6,),
+            borderRadius: const BorderRadius.all(Radius.circular(250),),
+            ),
+          ),
+        ],
+      ),
 
     );
   }
